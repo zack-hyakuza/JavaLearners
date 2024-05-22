bc823db946e6457bec0088a9e51254712c04671c
GSpisso <gabriel.spisso12@gmail.com>
2024-05-22 09:28:00 -0300

    Update create.php
    Coloquei o meu create

--- create.php
+++ create.php
@@ -2,20 +2,21 @@
 include 'functions.php';
 $pdo = pdo_connect_mysql();
 $msg = '';
+
 // Check if POST data is not empty
 if (!empty($_POST)) {
     // Post data not empty insert a new record
-    // Set-up the variables that are going to be inserted, we must check if the POST variables exist if not we can default them to blank
-    $id = isset($_POST['id']) && !empty($_POST['id']) && $_POST['id'] != 'auto' ? $_POST['id'] : NULL;
-    // Check if POST variable "name" exists, if not default the value to blank, basically the same for all variables
+    // Set-up the variables that are going to be inserted
     $name = isset($_POST['name']) ? $_POST['name'] : '';
     $email = isset($_POST['email']) ? $_POST['email'] : '';
     $phone = isset($_POST['phone']) ? $_POST['phone'] : '';
     $title = isset($_POST['title']) ? $_POST['title'] : '';
     $created = isset($_POST['created']) ? $_POST['created'] : date('Y-m-d H:i:s');
+    
     // Insert new record into the contacts table
-    $stmt = $pdo->prepare('INSERT INTO contacts VALUES (?, ?, ?, ?, ?, ?)');
-    $stmt->execute([$id, $name, $email, $phone, $title, $created]);
+    $stmt = $pdo->prepare('INSERT INTO contacts (name, email, phone, title, created) VALUES (?, ?, ?, ?, ?)');
+    $stmt->execute([$name, $email, $phone, $title, $created]);
+    
     // Output message
     $msg = 'Created Successfully!';
 }
@@ -24,21 +25,34 @@ if (!empty($_POST)) {
 <?=template_header('Create')?>
 
 <div class="content update">
-	<h2>Criar Contato</h2>
+    <h2>Create an Account</h2>
     <form action="create.php" method="post">
-        <label for="id">ID</label>
-        <label for="name">Nome</label>
-        <input type="text" name="id" placeholder="26" value="auto" id="id">
-        <input type="text" name="name" placeholder="John Doe" id="name">
-        <label for="email">Email</label>
-        <label for="phone">Telefone</label>
-        <input type="text" name="email" placeholder="johndoe@example.com" id="email">
-        <input type="text" name="phone" placeholder="2025550143" id="phone">
-        <label for="title">Emprego</label>
-        <label for="created">Criado</label>
-        <input type="text" name="title" placeholder="Employee" id="title">
-        <input type="datetime-local" name="created" value="<?=date('Y-m-d\TH:i')?>" id="created">
-        <input type="submit" value="Criar">
+        <div class="input-group">
+            <label for="name">Name</label>
+<div>
+            <input type="text" name="name" id="name" placeholder="John Doe"><br>
+        </div>
+        <div class="input-group">
+            <label for="email">Email</label>
+<div>
+            <input type="text" name="email" id="email" placeholder="johndoe@example.com"><br>
+        </div>
+        <div class="input-group">
+            <label for="phone">Phone</label>
+<div>
+            <input type="text" name="phone" id="phone" placeholder="2025550143"><br>
+        </div>
+        <div class="input-group">
+            <label for="title">Title</label>
+<div>
+            <input type="text" name="title" id="title" placeholder="Employee"><br>
+        </div>
+        <div class="input-group">
+            <label for="created">Created</label>
+<div>
+            <input type="datetime-local" name="created" id="created" value="<?=date('Y-m-d\TH:i')?>"><br>
+        </div>
+        <input type="submit" value="Create">
     </form>
     <?php if ($msg): ?>
     <p><?=$msg?></p>
