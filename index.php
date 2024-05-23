9aeb298ec1e87e15fa060da23b0e11aa5b3583dc
GSpisso <gabriel.spisso12@gmail.com>
2024-05-23 09:28:49 -0300

    "bem vindo" para "bem-vindo" e "Crie a sua conta" para "Crie sua conta"

--- index.php
+++ index.php
@@ -9,13 +9,13 @@ include 'functions.php';
 
 <div class="content">
 	<h2>Início</h2>
-	<p>Seja bem vindo ao Java Learners!</p>
+	<p>Seja bem-vindo ao Java Learners!</p>
 </div>
 
 <?=template_footer()?>
 
 <div class="content read">
-	<h2>Crie Sua Conta</h2>
+	<h2>Crie a Sua Conta</h2>
 	<a href="create.php" class="create-contact">Crie Sua Conta</a>
 	<table>
         <thead>
