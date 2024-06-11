d9d460bcde5c6cc858c7352ad2c717ad2198ac72
DaviNeymar <117181461+Davi-s1@users.noreply.github.com>
2024-06-11 16:59:31 -0300

    Update functions.php
    100% atualizado.

--- functions.php
+++ functions.php
@@ -1,16 +1,5 @@
 <?php
-function pdo_connect_mysql() {
-    $DATABASE_HOST = 'localhost';
-    $DATABASE_USER = 'root';
-    $DATABASE_PASS = '';
-    $DATABASE_NAME = 'phpcrud';
-    try {
-    	return new PDO('mysql:host=' . $DATABASE_HOST . ';dbname=' . $DATABASE_NAME . ';charset=utf8', $DATABASE_USER, $DATABASE_PASS);
-    } catch (PDOException $exception) {
-    	// If there is an error with the connection, stop the script and display the error.
-    	exit('Failed to connect to database!');
-    }
-}
+
 function template_header($title) {
 echo <<<EOT
 <!DOCTYPE html>
@@ -29,7 +18,6 @@ echo <<<EOT
     		<h1>Java Learners</h1>
             <a href="index.php"><i class="fas fa-home"></i>Início</a>
 			<a href="activities.php"><i class="fas fa-book"></i>Atividades</a>
-    		<a href="read.php"><i class="fas fa-address-book"></i>Contatos</a>
     	</div>
 		
     </nav>
