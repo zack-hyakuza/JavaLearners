aefc821c9552da35b3b07dc648efb6159aa1440b
DaviNeymar <117181461+Davi-s1@users.noreply.github.com>
2024-06-11 23:33:59 -0300

    Update conexao.php
    tirei o return null, tava la sem motivo nenhum.

--- conexao.php
+++ conexao.php
@@ -9,7 +9,6 @@ $mysqli = new mysqli($host, $usuario, $senha, $database);
 
 	if($mysqli->error){
 		die("Falha ao conectar ao banco de dados." . $mysqli->error);
-		return null;
 	}
 	else{
 		return $mysqli;
