f5c5974cf1af37c17f8fbe0946d29ebd55f0b123
DaviNeymar <117181461+Davi-s1@users.noreply.github.com>
2024-11-03 20:35:13 -0300

    Update functions.php
    03/11/2024

--- functions.php
+++ functions.php
@@ -1,44 +1,37 @@
 <?php
-function pdo_connect_mysql() {
-    $DATABASE_HOST = 'localhost';
-    $DATABASE_USER = 'root';
-    $DATABASE_PASS = '';
-    $DATABASE_NAME = 'phpcrud';
-    try {
-    	return new PDO('mysql:host=' . $DATABASE_HOST . ';dbname=' . $DATABASE_NAME . ';charset=utf8', $DATABASE_USER, $DATABASE_PASS);
-    } catch (PDOException $exception) {
-    	// If there is an error with the connection, stop the script and display the error.
-    	exit('Failed to connect to database!');
-    }
-}
-function template_header($title) {
-echo <<<EOT
+
+function template_header($title, $activePage) {
+    echo <<<EOT
 <!DOCTYPE html>
 <html>
 <link rel="icon" href="javalearners-logo.svg" type="image/x-icon">
-	<head>
-		<meta charset="utf-8">
-		<title>$title</title>
-		<link href="style.css" rel="stylesheet" type="text/css">
-		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
-	</head>
-	<body>
+<head>
 
-	 <nav class="navtop">
-    	<div>
-    		<h1>Java Learners</h1>
-            <a href="index.php"><i class="fas fa-home"></i>Início</a>
-			<a href="activities.php"><i class="fas fa-book"></i>Atividades</a>
-    		<a href="read.php"><i class="fas fa-address-book"></i>Contatos</a>
-    	</div>
-		
+    <meta charset="utf-8">
+    <title>$title</title>
+    <link href="estilo.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
+	<link href="https://fonts.googleapis.com/css2?family=Merriweather:wght@400;700&display=swap" rel="stylesheet">
+
+</head>
+<body>
+
+<header>
+    <nav class="navtop" role="navigation" aria-label="Main navigation">
+        <div>
+            <h1>Java Learners</h1>
+            <p><a href="index.php" class="<?= $activePage === 'home' ? 'active' : '' ?>"><i class="fas fa-home"></i> Início</a></p>
+            <p><a href="activities.php" class="<?= $activePage === 'activities' ? 'active' : '' ?>"><i class="fas fa-book"></i> Atividades</a></p>
+        </div>
     </nav>
+</header>
 EOT;
 }
 function template_footer() {
-echo <<<EOT
+    echo <<<EOT
     </body>
 </html>
 EOT;
 }
 ?>
+
